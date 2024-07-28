dd2a2a723ad6020a7181c658176e451f3567d53c
Diya Ghodasara <diyaghodasara@diyas-MacBook-Pro.local>
2024-07-28 21:46:08 +0530

    first commit

---
+++ src/main/java/com/project/finanacedashboardbackend/FinanaceDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.finanacedashboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanaceDashboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanaceDashboardBackendApplication.class, args);
+    }
+
+}
